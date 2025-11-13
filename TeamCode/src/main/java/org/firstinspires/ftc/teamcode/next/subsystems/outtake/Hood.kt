8d37dac23734a96f59733fe026d0404a2eb88d1e
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2025-11-13 16:03:00 -0500

    Update Hood.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/subsystems/outtake/Hood.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/subsystems/outtake/Hood.kt
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.next.subsystems.outtake
 
 import dev.nextftc.core.subsystems.Subsystem
 import dev.nextftc.hardware.impl.ServoEx
+import com.bylazar.configurables.annotations.Configurable
 
+@Configurable
 object Hood: Subsystem {
     private val hoodServo = ServoEx("flap")
 
