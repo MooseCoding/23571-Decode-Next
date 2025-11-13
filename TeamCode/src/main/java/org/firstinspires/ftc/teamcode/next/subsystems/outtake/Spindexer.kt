51160cbdd30e0443c7ea3293dbe56c128a339562
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2025-11-13 16:02:44 -0500

    Add Configurable annotation to Spindexer object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/subsystems/outtake/Spindexer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/subsystems/outtake/Spindexer.kt
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.next.subsystems.outtake
 
 import dev.nextftc.core.subsystems.Subsystem
+import com.bylazar.configurables.annotations.Configurable
 
+@Configurable
 object Spindexer: Subsystem {
 
 }
