fc2b0375c663429d563d1f453b09391a487f2db9
moosecoding <moosecoding@moosecoding.my.domain>
2026-01-13 17:35:05 -0500

    Test THis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/tuning/SlothTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/tuning/SlothTest.kt
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.next.tuning
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import dev.nextftc.ftc.NextFTCOpMode
 
 @TeleOp
-class SlothTest {
+class SlothTest: NextFTCOpMode() {
+    public override fun onInit() {
 
+    }
+
+    override fun waitForStart() {
+    }
 }
